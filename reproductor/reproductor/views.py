944af2b42d6e039d6c70e79510d3a7d7b5592ade
Ramiro Zein Contreras Rangel <ramirozeincontrerasrangel@gmail.com>
2024-03-05 19:09:21 -0600

    Primeros registros

--- reproductor/reproductor/views.py
+++ reproductor/reproductor/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.forms.models import model_to_dict
+from inicio.models import *
 
 # Create your views here.
+# Contiene la lógica para la página de inicio
+
 def inicio(peticion):
-    return render(peticion, 'inicio/inicio.html', {})
+    canciones = Cancion.objects.all()
+    return render(peticion, 'inicio/inicio.html', {"canciones": canciones})
 
